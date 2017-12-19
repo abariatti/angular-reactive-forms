ff751f56f99ae4877cb4d91b97c0719e6e2a66e3
Bariatti Arnaud <Arnaud.Bariatti@swisscom.com>
2017-12-19 16:28:41 +0100

    integration with angular-materials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Reactive Forms';
+  title = 'Angular Material Reactive Forms Starter';
+  selectedUser = {
+    name: 'Arnaud',
+    details: 'fgsdafsad',
+    admin: true,
+    cool: false
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, 
+         MatCardModule,
+         MatCheckboxModule,
+         MatDialogModule,
+         MatIconModule,
+         MatInputModule,
+         MatListModule,
+         MatMenuModule,
+         MatSelectModule,
+         MatSidenavModule,
+         MatSlideToggleModule,
+         MatTabsModule,
+         MatToolbarModule,
+         MatGridListModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { FormComponent } from '../pages/form/form.component';
 import { HomeComponent } from '../pages/home/home.component';
-import { MatInputModule, MatOptionModule, MatSelectModule, MatGridListModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,12 +30,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatIconModule,
     MatInputModule,
-    MatOptionModule, MatSelectModule, MatGridListModule,
+    MatListModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    FlexLayoutModule,
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
